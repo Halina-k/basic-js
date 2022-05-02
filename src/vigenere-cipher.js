fcc7d9b2a93a1c6b29af8c62740237699e997b6e
Halina Kulakova <kalisakulakova@gmail.com>
2022-05-02 12:46:18 +0200

    feat:solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(x) {
+    this.metod = x;
+    this.en = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('')
+    this.square = this.vigenereSquare()
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  vigenereSquare() {
+    let square = []
+    for (let i = 0; i < this.en.length; i++) {
+      square[i] = this.en.slice(i).concat(this.en.slice(0, i))
+    }
+    return square
   }
+  encrypt(message, key) {
+    if (message === undefined || key === undefined){
+      throw Error('Incorrect arguments!')
+    }
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    let j = 0
+    let result = []
+      for (let i = 0; i < message.length; i++) {
+        if (message[i].match(/[A-Z]/) === null) {
+          result.push((message[i]))
+        } else {
+           if (j >= key.length) {
+            j = 0
+          }
+          let indexCol = this.en.indexOf(key[j])
+          let indexRow = this.en.indexOf(message[i])
+          result.push(this.square[indexCol][indexRow])
+          j++
+        }
+      }
+    if (this.metod === true || this.metod === undefined) {
+      return result.join('')
+    } else if (this.metod === false) {
+      return result.reverse().join('')
+    }
+
+  }
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) {
+      throw Error('Incorrect arguments!')
+    }
+    message = message.toUpperCase()
+    key = key.toUpperCase()
+    let j = 0
+    let result = []
+    for (let i = 0; i < message.length; i++) {
+      if (message[i].match(/[A-Z]/) === null) {
+        result.push((message[i]))
+      } else {
+        if (j >= key.length) {
+          j = 0
+        }
+        let indexCol = this.en.indexOf(key[j])
+        let index = this.square[indexCol].indexOf(message[i])
+        result.push(this.en[index])
+        j++
+      }
+    }
+    if (this.metod === true || this.metod === undefined) {
+      return result.join('')
+    } else if (this.metod === false) {
+      return result.reverse().join('')
+    }
+   
+  }
+
 }
 
 module.exports = {
