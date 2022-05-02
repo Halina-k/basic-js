cca2a59752e9a148c4b7682ceabcdb388cb6c4d1
Halina Kulakova <kalisakulakova@gmail.com>
2022-05-02 23:30:31 +0200

    feat: solution

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,9 +23,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  let rez = []
+
+  for (i = 0; i < matrix.length + 2; i++) {
+    rez.push([])
+    for (j = 0; j < matrix[0].length + 2; j++)
+      rez[i].push(0)    
+  }
+
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === true) {
+        
+        for(let k = i - 1; k <= i + 1; k++) {
+          for (let d = j - 1; d <= j + 1; d++) {
+            if((k+1) === (i+1) && (d+1) === (j+1)) {
+              console.log('lkk')
+              rez[k + 1][d + 1] = rez[k + 1][d + 1]
+            } else {
+              rez[k + 1][d + 1]++
+            }
+          }
+        }
+        }
+      }
+    
+  }
+  let result = []
+  let resRow =  rez.slice(1, rez.length-1)
+  for (i = 0; i < resRow.length; i++){
+    itemCol = resRow[i].slice(1, resRow[i].length - 1)
+    result.push(itemCol)
+  }
+  return result
 }
 
 module.exports = {
